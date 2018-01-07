34e3825dc15c0534ae9c0fbb473c95f232901d40
frosty <desipher.ams@gmail.com>
2018-01-07 17:20:00 +0500

    Fix 19

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,9 @@ var jwt = require('koa-jwt')
 module.exports = function *(next) {
 
 	try {
+		console.log('Keys ' + Object.keys(this.request.fields));
 		var token = this.request.header['authorization'].split(" ")[1];
+		// console.log('Token ' + token);
 		// var token = this.get('authorization').split(" ")[1]; // this.response.get
 	} catch(err) {
 		var user = this.request.fields;
@@ -24,9 +26,9 @@ module.exports = function *(next) {
   				'Error': 'No response'
   			});
   		}
-			console.log('Decoded ' + decoded);
-			console.log('Token ' + token);
-			console.log('Keys ' + Object.keys(this.request.fields));
+			// console.log('Decoded ' + decoded);
+			// console.log('Token ' + token);
+			// console.log('Keys ' + Object.keys(this.request.fields));
   		yield next;
 		} catch(err) {
 			this.body = JSON.stringify({
